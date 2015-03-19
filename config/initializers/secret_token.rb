8b064e810e5fca9f1d4c21d88df006f11e15a365
rekha dhakad <rekhadhakad@cdnsol.com>
2015-03-19 19:16:34 +0530

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Terminatorsacademy::Application.config.secret_key_base = '8b3e72b712a5aec9b06856bd2b8458d18f4815054b94133bc432e73c9c9c014e001acd931eb7204600b231f3102ad4c1c067ae4e37f0ce6297d68fd707a81b29'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Terminatorsacademy::Application.config.session_store :cookie_store, key: '_terminatorsacademy_session'
